0a4936dc841166d8501fd4a68cb5959113d886b0
danilalukyanchenkoQA <danila.kaluga26@gmail.com>
2025-08-31 14:04:11 +0300

    wrote authorization tests

--- cypress/e2e/first-test.spec.ts
+++ cypress/e2e/first-test.spec.ts
@@ -1,5 +1,6 @@
 
 describe('Страница авторизации', (): void => {
+
   it('Открытие страницы авторизации', (): void => {
     cy.visit('/login'); 
     cy.contains('Welcome to Grafana').should('be.visible');
@@ -8,10 +9,29 @@ describe('Страница авторизации', (): void => {
   it('Проверка на введенный в строку текст', (): void => {
     cy.visit('/login');
     cy.contains('Welcome to Grafana').should('be.visible');
-    cy.get('input[name="user"]').type('your_username@example.com').should('have.value', 'your_username@example.com');
+    cy.get('input[name="user"]').type('your_username@example.com').should('have.value', 'your_username@example.com')
     cy.get('input[name="password"]').type('your_password')
     cy.get('button[aria-label="Show password"]').click()
-    cy.get('input[name="password"]').should('have.value', 'your_password');
+    cy.get('input[name="password"]').should('have.value', 'your_password')
   });
+
+  it('Вход с невалидным логином и паролем', (): void => {
+    cy.visit('/login');
+    cy.contains('Welcome to Grafana').should('be.visible')
+    cy.get('input[name="user"]').type('your_username@example.com')
+    cy.get('input[name="password"]').type('your_password')
+    cy.get('[data-testid="data-testid Login button"]').click()
+    cy.contains('Invalid username or password')
+  });
+
+    it('Незаполнены обязательные поля', (): void => {
+    cy.visit('/login');
+    cy.contains('Welcome to Grafana').should('be.visible')
+    cy.get('[data-testid="data-testid Login button"]').click()
+    cy.contains('Password is required')
+    cy.contains('Email or username is required')
+  });
+
+  
   
 });
