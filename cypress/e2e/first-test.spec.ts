84536a633cfb64e43adfe2ecf61a393f48f7b15b
danilalukyanchenkoQA <danila.kaluga26@gmail.com>
2025-08-31 13:19:39 +0300

    wrote case

--- cypress/e2e/first-test.spec.ts
+++ cypress/e2e/first-test.spec.ts
@@ -1,7 +1,17 @@
 
-describe('Первый тест на TypeScript', (): void => {
-  it('Должен открыть главную страницу', (): void => {
+describe('Страница авторизации', (): void => {
+  it('Открытие страницы авторизации', (): void => {
     cy.visit('http://localhost:3000/login');
     cy.contains('Welcome to Grafana').should('be.visible');
   });
+
+  it('Проверка на введенный в строку текст', (): void => {
+    cy.visit('http://localhost:3000/login');
+    cy.contains('Welcome to Grafana').should('be.visible');
+    cy.get('input[name="user"]').type('your_username@example.com').should('have.value', 'your_username@example.com');
+    cy.get('input[name="password"]').type('your_password')
+    cy.get('button[aria-label="Show password"]').click()
+    cy.get('input[name="password"]').should('have.value', 'your_password');
+  });
+  
 });
